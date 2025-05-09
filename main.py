887b0210424b0ae1efde892a25f35cc60bcaa2ab
pascal267 <bergrobert56@gmail.com>
2025-05-09 17:32:26 -0500

    Completed BookBot

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from stats import split
 from stats import letter_count
-split("books/frankenstein.txt")
-letter_count("books/frankenstein.txt")
+from stats import set_order
+import sys
+
+def main(blep):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {blep}")
+    print("----------- Word Count ----------")
+    split(blep)
+    print("--------- Character Count -------")
+    set_order(letter_count(blep))
+    print("============= END ===============")
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main(sys.argv[1])
--- stats.py
+++ stats.py
@@ -6,7 +6,7 @@ def get_book_text(path_to_book):
 def split(path_to_book):
     word_list = []
     word_list = get_book_text(path_to_book).split()
-    print(f"{len(word_list)} words found in the document")
+    print(f"Found {len(word_list)} total words")
     return len(word_list)
 
 def letter_count(text):
@@ -20,5 +20,16 @@ def letter_count(text):
                 letters[l] += 1
             else:
                 letters[l] = 1
-    print(letters)
     return letters
+
+def sort_on(s):
+    return s["num"]
+
+def set_order(inp):
+    dicl = []
+    for a, b in inp.items():
+        dics = {"char": a, "num": b}
+        dicl.append(dics)
+    dicl.sort(reverse=True, key=sort_on)
+    for j in dicl:
+        print(f"{j["char"]}: {j["num"]}")
